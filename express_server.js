46f3ddd0134509709cf55cc6940b19062d90f0c3
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-04-13 20:04:00 +0000

    added post method for submitting new long url

--- express_server.js
+++ express_server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+const generateRandomString = () => {
+  let result = '';
+  constcharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const count = 0;
+  while (count < 6){
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+    count++;
+  }
+  return result;
+};
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,7 +44,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_new", templateVars);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
