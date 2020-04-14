b8e8a677f17006ca37d1e3583e7f8b754b02e4af
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-04-14 21:03:14 +0000

    added login and logout features

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cookie = require('cookie-parser');
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookie());
 app.set('view engine', 'ejs');
 
 const generateRandomString = () => {
@@ -40,14 +42,25 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
+
+app.post("/login", (req, res) => {
+ const username = req.body.username;
+ res.cookie('username',username);
+ res.redirect('/urls');
+});
+
+app.post("/logout", (req, res) => {
+  const username = req.body.username;
+  res.clearCookie('username',username);
+  res.redirect('/urls');
+ });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
@@ -57,7 +70,10 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"]
+  };
   res.render("urls_new", templateVars);
 });
 
@@ -76,6 +92,11 @@ app.get("/u/:shortURL", (req, res) => {
 
 });
 
+app.post('/urls/:shortURL/delete', (req,res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls')
+});
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[ req.params.shortURL];
   if(longURL){
@@ -88,7 +109,11 @@ app.get("/u/:shortURL", (req, res) => {
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  let templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
